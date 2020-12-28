61f4304fbc5563c357507b92f4fcf1246751ca33
carametal <mkatsdevelop@gmail.com>
2020-12-28 11:53:34 +0900

    phpを7.4にアップデート

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum install -y httpd
-sudo amazon-linux-extras install php7.3
+sudo amazon-linux-extras install php7.4
 sudo yum localinstall https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm -y
 sudo yum-config-manager --disable mysql80-community
 sudo yum-config-manager --enable mysql57-community
