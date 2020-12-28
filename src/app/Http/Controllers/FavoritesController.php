f26ff06b0b60c1693a28b50d7d2d6b4cb11df4ff
carametal <mkatsdevelop@gmail.com>
2020-12-28 21:59:25 +0900

    お気に入りの重複登録と存在しないお気に入りの削除にガードを実装

--- src/app/Http/Controllers/FavoritesController.php
+++ src/app/Http/Controllers/FavoritesController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Rests\Favorites;
+use App\Models\Favorite;
 use Illuminate\Http\Request;
+use Throwable;
 
 class FavoritesController extends Controller
 {
@@ -21,10 +22,18 @@ class FavoritesController extends Controller
     public function store(Request $request)
     {
         try {
-            $favorites = new Favorites();
-            return json_encode(['favorite' => $favorites->create($request)]);
-        } catch (Exception $e) {
-            throw $e;
+            if(0 < Favorite::where('user_id', $request->userId)->where('favorite_tweet_id', $request->tweetId)->count())
+            {
+                return response()->json(['errorCode' => 1, 'message' => 'already favorited'], 403);
+            }
+            $favorite = Favorite::factory()->create([
+                'user_id' => $request->userId,
+                'favorite_tweet_id' => $request->tweetId
+            ]);
+            echo "success\n";
+            return json_encode(['favorite' => $favorite]);
+        } catch (Throwable $th) {
+            throw $th;
         }
     }
 
@@ -37,10 +46,14 @@ class FavoritesController extends Controller
     public function destroy($id)
     {
         try {
-            $favorites = new Favorites();
-            return json_encode(['favorite' => $favorites->delete($id)]);
-        } catch (Exception $e) {
-            throw $e;
+            $favorite = Favorite::find($id);
+            if($favorite == null)
+            {
+                return response()->json(['errorCode' => 1, 'message' => 'can not find favorite.'], 403);
+            }
+            return json_encode(['favorite' => $favorite->delete($favorite)]);
+        } catch (Throwable $th) {
+            throw $th;
         }
     }
 }
--- src/tests/Feature/FavoriteTest.php
+++ src/tests/Feature/FavoriteTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Favorite;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Str;
 use App\Models\User;
 use App\Models\Tweet;
 use Tests\TestCase;
@@ -43,10 +43,36 @@ class FavoriteTest extends TestCase
     public function testFavoriteRemove()
     {
         $tweet = Tweet::factory()->create();
-        $response = $this->delete("favorites/$tweet->id");
+        $favorite = Favorite::factory()->create([
+            'user_id' => $this->user->id,
+            'favorite_tweet_id' => $tweet->id
+        ]);
+        $response = $this->delete("favorites/{$favorite->id}");
         $response->assertStatus(200);
         $this->assertDatabaseMissing('favorites', [
             'id' => $tweet->id
         ]);
+    }
+
+    public function testDuplicateFavorite()
+    {
+        $tweet = Tweet::factory()->create();
+        Favorite::factory()->create([
+            'user_id' => $this->user->id,
+            'favorite_tweet_id' => $tweet->id
+        ]);
+        $response = $this->post('favorites', [
+            'userId' => $this->user->id,
+            'tweetId' => $tweet->id
+        ]);
+        $response->assertStatus(403);
+        $this->assertEquals($response->json()['errorCode'], 1);
+    }
+
+    public function testFavoriteNotExists()
+    {
+        $response = $this->delete('favorites/9999');
+        $response->assertStatus(403);
+        $this->assertEquals($response->json()['errorCode'], 1);
     }
 }
