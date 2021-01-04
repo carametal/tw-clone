5f6b1510eff47e902dde1b9c58fe7d26cee69e3c
carametal <mkatsdevelop@gmail.com>
2021-01-04 19:06:39 +0900

    フォロー数とフォロワー数のリンク先がログインユーザーのユーザーホームになっているバグを修正

--- src/resources/js/components/UserDetailCard.js
+++ src/resources/js/components/UserDetailCard.js
@@ -12,9 +12,9 @@ export default function UserDetailCard(props) {
     <Card>
       <Card.Body style={{ borderBottom: "1px solid rgba(0, 0, 0, 0.125)"}}>
         <h3>{ props.user.name }</h3>
-        <div><a href={"/users/" + _loginUser.id} style={{color:"black"}}>ツイート数: { userDetail.count || 0}</a></div>
-        <div><a href={"/follow-list/" + _loginUser.id} style={{color:"black"}}>フォロー: { userDetail.follows || 0}</a></div>
-        <div><a href={"/follower-list/" + _loginUser.id} style={{color:"black"}}>フォロワー: { userDetail.followers || 0}</a></div>
+        <div><a href={"/users/" + props.user.id} style={{color:"black"}}>ツイート数: { userDetail.count || 0}</a></div>
+        <div><a href={"/follow-list/" + props.user.id} style={{color:"black"}}>フォロー: { userDetail.follows || 0}</a></div>
+        <div><a href={"/follower-list/" + props.user.id} style={{color:"black"}}>フォロワー: { userDetail.followers || 0}</a></div>
         <div>{props.user.bio}</div>
       </Card.Body>
     </Card>
